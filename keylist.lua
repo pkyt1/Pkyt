10800f79960432e42f2004485e3603113508fe8e
pkyt1 <pkleaderhacker@gmail.com>
2026-09-10 02:02:10 +0500

    Update keylist.lua

--- keylist.lua
+++ keylist.lua
@@ -41,7 +41,7 @@ return {
         ["PKLEADER"] = {
         type = "DEMO",
         expiry = "2026-09-8",
-        valid = true,
+        valid = false,
         max_devices = 2,
         SLOT = "2"
     },
