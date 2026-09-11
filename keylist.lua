3495fa460652d3bcde1576ee2b6bd49624dd54f2
pkyt1 <pkleaderhacker@gmail.com>
2026-09-11 13:18:58 +0500

    Add LEADER-VIP-7D entry to keylist

--- keylist.lua
+++ keylist.lua
@@ -24,6 +24,13 @@ return {
         max_devices = 1,
         SLOT = "2"
     },
+        ["LEADER-VIP-7D"] = {
+        type = "DEMO",
+        expiry = "2026-09-18",
+        valid = true,
+        max_devices = 1,
+        SLOT = "2"
+    },
         ["LEADER7DAYS"] = {
         type = "DEMO",
         expiry = "2026-09-17",
